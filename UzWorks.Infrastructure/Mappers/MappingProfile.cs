4070448e3d585b42c3f7a368c6c5cbd7635b0aa5
Abdulaziz <97102064+BatchatPro@users.noreply.github.com>
2023-11-17 10:45:02 +0500

    All AutoMappers Created in Uzworks.Infrastructure

---
+++ UzWorks.Infrastructure/Mappers/MappingProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using UzWorks.Core.DataTransferObjects.JobCategories;
+using UzWorks.Core.DataTransferObjects.Jobs;
+using UzWorks.Core.DataTransferObjects.Location.Districts;
+using UzWorks.Core.DataTransferObjects.Location.Regions;
+using UzWorks.Core.DataTransferObjects.Workers;
+using UzWorks.Core.Entities.JobAndWork;
+using UzWorks.Core.Entities.Location;
+
+namespace UzWorks.Infrastructure.Mappers;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile() 
+    {
+        CreateMap<JobCategory, JobCategoryVM>().ReverseMap();
+        CreateMap<JobCategory, JobCategoryDto>().ReverseMap();
+
+        CreateMap<District, DistrictDto>().ReverseMap();
+        CreateMap<District, DistrictVM>().ReverseMap();
+
+        CreateMap<Region,RegionDto>().ReverseMap();
+        CreateMap<Region,RegionVM>().ReverseMap();
+
+        CreateMap<Worker,WorkerDto>().ReverseMap();
+        CreateMap<Worker,WorkerVM>().ReverseMap();
+
+        CreateMap<Job,JobDto>().ReverseMap();
+        CreateMap<Job,JobVM>().ReverseMap();
+    }
+}
---
+++ UzWorks.Infrastructure/Mappers/MappingService.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using UzWorks.Core.Abstract;
+
+namespace UzWorks.Infrastructure.Mappers;
+
+public class MappingService : IMappingService
+{
+    private readonly IMapper mapper;
+
+    public MappingService(IMapper mapper)
+    {
+        this.mapper = mapper;
+    }
+
+    public T Map<T, TSourse>(TSourse sourse)
+    {
+        return mapper.Map<T>(sourse);
+    }
+
+    public T Map<T, TSource>(TSource source, T destination)
+    {
+        mapper.Map(source, destination);
+        return destination;
+    }
+}
