7b320762814d082222471a1205fa6813eed5c4dc
Abdulaziz <97102064+BatchatPro@users.noreply.github.com>
2023-11-24 14:48:21 +0500

    Destict and Region Controllers ready

---
+++ UzWorks/Controllers/DistrictController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UzWorks.BL.Services.Locations.Districts;
+using UzWorks.Core.DataTransferObjects.Location.Districts;
+
+namespace UzWorks.API.Controllers;
+
+public class DistrictController : BaseController
+{
+    private readonly IDistrictService _districtService;
+    public DistrictController(IDistrictService districtService)
+    {
+        _districtService = districtService;
+    }
+
+    [HttpPost] 
+    public async Task<IActionResult> Create([FromBody]DistrictDto district)
+    {
+        var result = await _districtService.Create(district);
+        return Ok(result);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete([FromRoute]Guid id)
+    {
+        await _districtService.Delete(id);
+        return Ok();
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Edit([FromBody]DistrictEM districtEM)
+    {
+        var result = await _districtService.Update(districtEM);
+        return Ok(result);
+    }
+
+    [AllowAnonymous]
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById([FromBody]Guid id)
+    {
+        var result = await _districtService.GetById(id);
+        return Ok(result);
+    }
+
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = _districtService.GetAllAsync();
+        return Ok(result);
+    }
+
+    [AllowAnonymous]
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetByRegionId([FromRoute]Guid regionId)
+    {
+        var result = await _districtService.GetDistrictByRegionId(regionId);
+        return Ok(result);
+    }
+}
---
+++ UzWorks/Controllers/RegionController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UzWorks.BL.Services.Locations.Regions;
+using UzWorks.Core.DataTransferObjects.Location.Regions;
+
+namespace UzWorks.API.Controllers;
+
+public class RegionController : BaseController
+{
+    private readonly IRegionsService _regionsService;
+
+    public RegionController(IRegionsService regionsService)
+    {
+        _regionsService = regionsService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(RegionDto regionDto)
+    {
+        var result = await _regionsService.Create(regionDto);
+        return Ok(result);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete([FromRoute] Guid id)
+    {
+        await _regionsService.Delete(id);
+        return Ok();
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Edit([FromBody]RegionEM regionEM)
+    {
+        var result = await _regionsService.Update(regionEM);
+        return Ok(result);
+    }
+
+    [AllowAnonymous]
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById([FromRoute]Guid id)
+    {
+        var result = await _regionsService.GetById(id);
+        return Ok(result);
+    }
+
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = await _regionsService.GetAllAsync();
+        return Ok(result);
+    }
+}
