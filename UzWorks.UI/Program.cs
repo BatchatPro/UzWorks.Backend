15cbf3ec166cea41cc17aa6e8107e268c462739d
Abdulaziz <97102064+BatchatPro@users.noreply.github.com>
2024-01-02 18:38:07 +0500

    blazor project add for UI

---
+++ UzWorks.UI/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.ResponseCompression;
+using UzWorks.UI.Components;
+using UzWorks.UI.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+          new[] { "application/octet-stream" });
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseResponseCompression();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.MapHub<ChatHub>("/chathub");
+
+app.Run();
